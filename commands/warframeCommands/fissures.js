1b0c0a460927696f370f5a27bc7ba6dfa27263ae
Random-nii-chan <leokolo@hotmail.fr>
2018-12-10 01:15:37 +0100

    fissures trie les fissures par tier croissant

--- commands/warframeCommands/fissures.js
+++ commands/warframeCommands/fissures.js
@@ -1,5 +1,18 @@
 module.exports.run = async(client, message, args) => {
-	message.channel.send("fissures");
+	const util = require("./util.js");
+	const WorldState = require('warframe-worldstate-parser');
+	const worldstateData = await (require('request-promise'))('http://content.warframe.com/dynamic/worldState.php');
+	const ws = new WorldState(worldstateData);
+
+	var voidFissures = ws.fissures;
+	voidFissures.sort((a,b) => a.tierNum - b.tierNum);
+
+	var totalFissures = ws.fissures.length;
+	var generatedTime = util.timeString(ws.timestamp);
+	var string = `= Etat des fissures (${generatedTime}): ${totalFissures} fissures actives =\n`;
+	for(var i=0; i<totalFissures; i++) string+=util.fissureInfo(voidFissures[i]);
+
+	message.channel.send(util.convertAsciidoc(string));
 }
 
 module.exports.config = {
--- commands/warframeCommands/voidtrader.js
+++ commands/warframeCommands/voidtrader.js
@@ -1,6 +1,5 @@
 module.exports.run = async(client, message, args) => {
 	const util = require("./util.js");
-	//Worldstate
 	const WorldState = require('warframe-worldstate-parser');
 	const worldstateData = await (require('request-promise'))('http://content.warframe.com/dynamic/worldState.php');
 	const ws = new WorldState(worldstateData);
