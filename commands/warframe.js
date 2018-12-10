fef7106c3cea53d09bfcddcfa984b57b1051f3f1
Random-nii-chan <leokolo@hotmail.fr>
2018-12-10 01:35:14 +0100

    la gestion des commandes warframe par fs fonctionne

--- commands/warframe.js
+++ commands/warframe.js
@@ -8,8 +8,6 @@ module.exports.run = async(client, message, args) => {
 	//Déclaration des bases de données JSON
 	const config = require("../json/config.json");
 	//Déclaration de la requête URL
-	
-	//const market = require("./warframeCommands/marketPlace.js");
 
 	if(args[0].match(/sellers[0-9]*/gm)) var element = "sellers";
 	else var element = args[0];
@@ -17,51 +15,6 @@ module.exports.run = async(client, message, args) => {
 	var cmd = client.warcommands.get(element);
 	if(cmd) cmd.run(client, message, args);
 	else message.channel.send("Erreur de commande");
-	
-	/*
-		switch(element)
-		{
-			case 'voidtrader':
-				message.channel.send(util.voidTrader(ws.voidTrader,ws.timestamp));
-			break;
-
-			case 'find':
-				if(!args[1]) return message.channel.send("Renseignez une recherche !");
-				var search = util.stringifyArray(args.slice(1));
-				market.find(client,message,search);
-			break;
-			
-			//supprimer l'underscore dans la regex
-			case String(element.match(/sellers[0-9]*_/gm)):
-
-				if(args[0].slice(7) == "") var page = 1;
-				else var page = args[0].slice(7);
-
-				if(!args[1]) return message.channel.send("Renseignez un objet à rechercher !");
-				var search = util.urlConvert(args.slice(1));
-
-				market.sellers(client, message, search, page);
-			break;
-
-			case 'fissures':
-				var totalFissures = ws.fissures.length;
-				var generatedTime = util.timeString(ws.timestamp);
-				var string = `= Etat des fissures (${generatedTime}): ${totalFissures} fissures actives =\n`;
-				for(var i=0; i<totalFissures; i++) string+=util.fissureInfo(ws.fissures[i]);
-
-				message.channel.send(util.convertAsciidoc(string));
-			break;
-
-			case 'help':
-				message.channel.send(util.commandList());
-			break;
-
-			default:
-				message.channel.send("Commande inconnue.\nEffectuez `"+config.prefix+"warframe` pour afficher la liste des commandes disponibles pour Warframe.");
-			break;
-		}
-		Normalement ça devrait foirer
-	*/
 }
 
 module.exports.config = {
--- commands/warframeCommands/find.js
+++ commands/warframeCommands/find.js
@@ -3,22 +3,20 @@ const util = require("./util.js");
 
 //module.exports.run = async(client,message,search) => {
 module.exports.run = async(client,message,args) => {
+	if(!args[1]) return message.channel.send("Renseignez une recherche !");
 
+	var search = util.stringifyArray(args.slice(1));
 	var apiTarget = 'https://api.warframe.market/v1/items';
-	message.channel.send("find");
-	/*
-		request(apiTarget, function(error, response, body){
-			if (!error && response.statusCode == 200) {
-				var data = JSON.parse(body).payload.items.en;
-				message.channel.send(util.itemList(data,search));
-			}
-
-			else {
-				console.log(error);
-				message.channel.send("Une erreur s'est produite lors de l'accès à warframe.market.");
-			}
-		});
-	*/
+	request(apiTarget, function(error, response, body){
+		if (!error && response.statusCode == 200) {
+			var data = JSON.parse(body).payload.items.en;
+			message.channel.send(util.itemList(data,search));
+		}
+		else {
+			console.log(error);
+			message.channel.send("Une erreur s'est produite lors de l'accès à warframe.market.");
+		}
+	});
 }
 
 module.exports.config = {
