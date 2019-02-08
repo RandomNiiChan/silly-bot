3c90714531b39a55ec7139d5b4015e6e5785b4ee
randomniichan <leokolo@hotmail.fr>
2019-02-08 18:31:31 +0100

    début des classes loadout

---
+++ commands/mhwCommands/classes.js
@@ -0,0 +1,41 @@
+const request = require("request");
+const util = require("./util.js");
+const Discord = require("discord.js");
+const sqlite3 = require("sqlite3");
+
+class Loadout {
+	constructor(name, user) {
+		this.userId = user.id;
+		this.name = name;
+		this.id = this.generateId();
+	}
+
+	get id() {
+		return this._id;
+	}
+
+	set id(id) {
+		this._id = id;
+	}
+
+	generateId() {
+		return Math.floor(Math.random()*16777215).toString(16);
+	}
+
+	toString() {
+		return `${this.userId} - ${this.name} - ${this.id}`;
+	}
+}
+
+module.exports = {
+	Loadout: Loadout
+}
+
+module.exports.run = async(client, message, args) => {
+	//Cette commande doit rester vide
+	//C'est un fichier qui contient des commandes utiles
+}
+
+module.exports.config = {
+	command: "classes"
+}
--- commands/mhwCommands/loadout.js
+++ commands/mhwCommands/loadout.js
@@ -2,29 +2,39 @@ const request = require("request");
 const util = require("./util.js");
 const Discord = require("discord.js");
 const sqlite3 = require("sqlite3");
+const classes = require("./classes.js");
+const Loadout = classes.Loadout;
 
 module.exports.run = async(client,message,args) => {
-	let db = new sqlite3.Database('../../databases/mhw.sqlite', sqlite3.OPEN_READWRITE, (err) => {
-		if (err) console.error(err.message);
-		console.log('Successfully opened the database.');
-	});
-
+	var loadoutsDatabase = new sqlite3.Database('../../databases/mhw.sqlite');
 	var action = args[1];
 	switch(action)
 	{
 		case 'create':
-			
+			if(!args[2]) message.channel.send("Please input a name for your loadout !");
+			else
+			{
+				var arrayName = args.slice(2);
+				var loadoutName = util.stringifyArray(arrayName).toLowerCase();
+				console.log(loadoutName);
+			}
 		break;
 
 		case 'delete':
+			
+		break;
 
+		case 'list':
+			var test = new Loadout("albert",message.author);
+			message.channel.send(test.toString());
+		break;
+
+		default:
+			message.channel.send("Samarchpa");
 		break;
 	}
 
-	db.close((err) => {
-		if(err) console.error(err.message);
-		console.log('Successfully closed the database.');
-	});
+	loadoutsDatabase.close();
 }
 
 module.exports.config = {
