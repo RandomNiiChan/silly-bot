9eb0cee0d1ba6f66d941544a5f147e1bb2bd064e
randomniichan <leokolo@hotmail.fr>
2019-02-09 15:58:27 +0100

    Insertion loadout et refonte loadout list

--- commands/mhwCommands/classes.js
+++ commands/mhwCommands/classes.js
@@ -8,6 +8,7 @@ class Loadout {
 		this.userId = user.id;
 		this.name = name;
 		this.id = this.generateId();
+		this.note = " ";
 	}
 
 	get id() {
@@ -18,6 +19,22 @@ class Loadout {
 		this._id = id;
 	}
 
+	get userId() {
+		return this._userId;
+	}
+
+	set userId(userId) {
+		this._userId = userId;
+	}
+
+	get note() {
+		return this._note;
+	}
+
+	set note(note) {
+		this._note = note;
+	}
+
 	generateId() {
 		return Math.floor(Math.random()*16777215).toString(16);
 	}
@@ -26,8 +43,30 @@ class Loadout {
 		return `${this.userId} - ${this.name} - ${this.id}`;
 	}
 
+	registerLoadout(database) {
+		var argsArray = [this.userId,this.name];
+		database.run("INSERT INTO Loadouts(userId,name) VALUES(?,?)", argsArray, function(err) {
+			if(err) return console.log(err.message);
+			console.log(`Insertion d'un loadout. Id: ${this.lastID}`);
+		});
+	}
+
 	static rowToString(row) {
-		return `${row.name} - Contient: xxx, xxx, xxx\n`;
+		return `${row.name} - ${Loadout.getSlots(row)}\n`;
+	}
+
+	static getSlots(row) {
+		var usedSlots = new Array();
+		if(row.weapon) usedSlots.push("weapon");
+		if(row.charm) usedSlots.push("charm");
+		if(row.head) usedSlots.push("head");
+		if(row.chest) usedSlots.push("chest");
+		if(row.gloves) usedSlots.push("gloves");
+		if(row.waist) usedSlots.push("waist");
+		if(row.legs) usedSlots.push("legs");
+
+		if(usedSlots.length == 0) return `Loadout is empty. [${row.note}]`;
+		else return `Contains: ${usedSlots.join(", ")}. [${row.note}]`
 	}
 }
 
