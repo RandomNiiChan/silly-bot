e36ae94418cb498464384615b8ffb10bca9bb891
randomniichan <leokolo@hotmail.fr>
2019-02-27 15:29:44 +0100

    remise à zéro des loadouts + ajout des classes

--- commands/mhwCommands/classes.js
+++ commands/mhwCommands/classes.js
@@ -4,46 +4,6 @@ const Discord = require("discord.js");
 const sqlite3 = require("sqlite3");
 
 class Loadout {
-	constructor(name, user) {
-		this.userId = user.id;
-		this.name = name;
-		this.note = "";
-	}
-
-	get userId() {
-		return this._userId;
-	}
-
-	set userId(userId) {
-		this._userId = userId;
-	}
-
-	get note() {
-		return this._note;
-	}
-
-	set note(note) {
-		this._note = note;
-	}
-
-	registerLoadout(database, channel) {
-		var argsArray = [this.userId,this.name,this.note];
-
-		database.get("SELECT * FROM Loadouts WHERE userId = ? AND name = ?",[this.userId,this.name], function(err, row) {
-			if(!row) 
-				database.run("INSERT INTO Loadouts(userId,name,note) VALUES(?,?,?)", argsArray, function(err) {
-					if(err) return console.log(err.message);
-					console.log(`Insertion d'un loadout`);
-					channel.send("You created a new loadout: "+argsArray[1]);
-				});
-			else channel.send("You already created a loadout using this name.");
-		});
-	}
-
-	static rowToString(row) {
-		return `${row.name} - ${Loadout.getSlots(row)}\n`;
-	}
-
 	static getSlots(row) {
 		var usedSlots = new Array();
 		if(row.weapon) usedSlots.push("weapon");
