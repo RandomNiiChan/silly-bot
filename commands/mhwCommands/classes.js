ba31834c230e207014a55f3c23ee9d57172a9df8
randomniichan <leokolo@hotmail.fr>
2019-03-01 13:45:08 +0100

    abandon des loadouts

--- commands/mhwCommands/classes.js
+++ commands/mhwCommands/classes.js
@@ -4,6 +4,50 @@ const Discord = require("discord.js");
 const sqlite3 = require("sqlite3");
 
 class Loadout {
+	constructor(name,owner,row) {
+		if(name!=null && owner != null && row == null) {
+			this.owner = owner;
+			this.name = name;
+
+			this.note = null;
+			this.weapon = null;
+			this.charm = null;
+			this.head = null;
+			this.chest = null;
+			this.gloves = null;
+			this.waist = null;
+			this.legs = null;
+
+			this.id = null;
+		}
+		else if(row != null) {
+			this.name = row.name;
+			this.owner = row.userId;
+			
+			this.note = row.note;
+			this.weapon = row.weapon;
+			this.charm = row.charm;
+			this.head = row.head;
+			this.chest = row.chest;
+			this.gloves = row.gloves;
+			this.waist = row.waist;
+			this.legs = row.legs;
+
+			this.id = row.loadoutId;
+		}
+	}
+
+	insertInto(database) {
+		var args = [this.owner, this.name, ""]
+		database.run("INSERT INTO Loadouts(userId,name,note) VALUES(?,?,?)", args, function(err) {
+			if(err) {
+				message.channel.send("An error has occured, OOF");
+				return console.log(err.message);
+			}
+			console.log("A new loadout has been created, id: "+this.lastID);
+		});
+	}
+
 	static getSlots(row) {
 		var usedSlots = new Array();
 		if(row.weapon) usedSlots.push("weapon");
@@ -17,10 +61,25 @@ class Loadout {
 		if(usedSlots.length == 0) return `Loadout is empty. [${row.note}]`;
 		else return `Contains: ${usedSlots.join(", ")}. [${row.note}]`
 	}
+
+	toString() {
+		Loadout.getSlots(this)
+	}
+}
+
+class Wearable {
+	constructor(object) {
+		this.availableSlots = object.slots;
+	}
+
+	toString() {
+
+	}
 }
 
 module.exports = {
-	Loadout: Loadout
+	Loadout: Loadout,
+	Wearable: Wearable
 }
 
 module.exports.run = async(client, message, args) => {
